b3cfd473cbe6e81eb7ff0f441c47198f285eaf87
Zak G <zakgriffith@gmail.com>
2024-08-02 10:33:04 -0600

    copy: Nodes -> Batches

--- packages/nextjs/components/homepage/Welcome.tsx
+++ packages/nextjs/components/homepage/Welcome.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const Welcome = () => {
   return (
     <div className="md:text-base text-[0.8rem] border rounded-xl p-4 text-center">
-      <h1 className="font-bold font-typo-round md:text-2xl text-xl ">BuidlGuild Workshop Streams</h1>
+      <h1 className="font-bold font-typo-round md:text-2xl text-xl ">BuidlGuild Batches</h1>
       <p>
-        We&apos;re funding BuidlGuidl members for building and maintaining our Nodes program. We are creating scripts to
-        quickly create and monitor nodes, building and shipping synced nodes to underserved areas, and providing RPC
-        access.
+        We&apos;re funding BuidlGuidl members for their contributions to the management, maintenance, and expansion of
+        the BG Batches program. The next level in a builder&apos;s journey after SpeedRunEthereum, builders in the
+        program will learn to build and collaborate around a GitHub repo using best practices.
       </p>
     </div>
   );
